c7bfabe6401fee2d6aa8e6005b96ca93d27d39ec
adityasunny1189 <adityapathak1189@gmail.com>
2019-06-29 03:45:51 +0530

    js notes for basic

---
+++ notes.js
@@ -0,0 +1,8 @@
+//USED TO PRINT MESSAGE IN THE CONSOLE
+console.log("hello world");
+
+//USED TO GIVE A ALERT MESSAGE IN THE BROWSER
+alert("welcome");
+
+//USED TO PROMPT USER FOR input
+prompt("your name");
