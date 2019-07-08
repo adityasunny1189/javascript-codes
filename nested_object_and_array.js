a30b0b9bee04cdd8e9560dcb010b2cbf911f7a2b
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-08 09:50:58 +0530

    nested obj arr

---
+++ nested_object_and_array.js
@@ -0,0 +1,11 @@
+let obj1={
+  name:"aditya",
+  branch:"ISE",
+  college:"SIT",
+  friends:["rohan","rohit","saksham"],
+  what_knows:["C programming",python:{
+    numpy:"little bit",
+    pandas:"complete",
+    openCV:"intermediate"
+  },"javascript","arduino"]
+};
