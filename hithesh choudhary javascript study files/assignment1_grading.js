1787177d88a24d9d18cfe76b8d3fced228b45175
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-09 14:45:56 +0530

    LCO

---
+++ hithesh choudhary javascript study files/assignment1_grading.js
@@ -0,0 +1,15 @@
+//Grading system
+let marks = 79;
+
+if(marks === 100 || marks >= 90)
+{
+    console.log("pass");
+}
+else if(marks <= 90)
+{
+    console.log("fail");
+}
+else
+{
+    console.log("Not Appeared");
+}
---
+++ hithesh choudhary javascript study files/lecture2_celcius.js
@@ -0,0 +1,22 @@
+//Arithmatic operation
+var num1 = 100;
+
+var num2 = 200;
+
+var total = (num1 + num2) * 0.98;
+
+console.log(total);
+
+//temperature conversion
+
+//take input temperature in Farenhite
+
+let TempInFar = 100;
+
+//converting temperature in Farenhite to celcius
+
+let TempInCel = (TempInFar - 32) * (5/9);
+
+//printing temperature in celcius
+
+console.log(TempInFar + ' degree in Farenhite is ' + TempInCel + ' degree in celcius');
