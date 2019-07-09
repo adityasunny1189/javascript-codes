3437883243c9bc44766286422d05af6e3cff34fc
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-10 01:53:14 +0530

    count of char in string

--- hithesh choudhary javascript study files/practice_count_of_char_in_str.js
+++ hithesh choudhary javascript study files/practice_count_of_char_in_str.js
@@ -2,7 +2,7 @@
 //returning the count in object form 
 
 //declaring a string
-var string1 = 'aditya';
+var string1 = 'Aditya';
 
 //declaring an empty object
 var result = {};
@@ -11,7 +11,8 @@ var result = {};
 for(var i=0; i<string1.length; i++)
 {
     //containg the character in a variable
-    var char1 = string1[i];                                                 // n assignment
+    //converting the string in lower case
+    var char1 = string1[i].toLowerCase();                                   // n assignment
 
     //testing the if condition if that character is there in the object
     //if the character is present than increase its count 
