fff0fd00c4250240eee7a401b7fd3ba8ed83a617
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-09 12:30:37 +0530

    LCO variables

---
+++ LCO_variable.js
@@ -0,0 +1,31 @@
+//variable name start with keyword let
+//variable name also starts with var keyword
+//we can only declare a variable once 
+//means if we again declare that variable name then error will come
+
+
+let firstName = 'aditya';
+
+let lastName = 'pathak';
+
+let score = 100;
+
+console.log(score);
+
+//changing variable value
+
+score = 99;
+
+let bonous1 = '20';
+
+let bonous2 = '100';
+
+let total = bonous1 + bonous2;//two strings are concatenated 
+
+console.log(firstName + ' ' + lastName);
+
+console.log(score);
+
+console.log(total);//concatenation of two string
+
+//constant start with keyword const
---
+++ hithesh choudhary javascript study files/LCO_variable.js
@@ -0,0 +1,25 @@
+//variable name start with keyword let
+//variable name also starts with var keyword
+//we can only declare a variable once 
+//means if we again use that variable name then error will come
+//unlike C programming
+
+let firstName = 'aditya';
+
+let lastName = 'pathak';
+
+let score = 100;
+
+let bonous1 = '20';
+
+let bonous2 = '100';
+
+let total = bonous1 + bonous2;//two strings are concatenated 
+
+console.log(firstName + ' ' + lastName);
+
+console.log(score);
+
+console.log(total);//concatenation of two string
+
+//constant start with keyword const
