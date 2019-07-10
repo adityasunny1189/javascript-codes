21bdef5d9a6e76bc4f72458278d8993f6be8a7f9
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-11 00:19:18 +0530

    array in js

---
+++ hithesh choudhary javascript study files/lecture4_marvels.js
@@ -0,0 +1,38 @@
+//                                                |-------|
+//                                                | Array |
+//                                                |-------|
+//New Datatype === Array in this lecture
+//usually we use const keyword for declaring array
+
+//declaring an array by []
+
+const marvel_Heros = ['Iron Man','SpiderMan','Capt. America'];
+
+//not only string we can also store a bunch of diffrent datatypes in Js arrays
+
+//valid js array
+
+const Test_array = ['aditya', 100, true, null];
+
+//accesing an array
+
+console.log(marvel_Heros);
+
+//accesing each element by index no
+//index start at position 0
+
+console.log(marvel_Heros[0]);
+
+//Iron Man will be printed
+//because Iron Man is at index 0
+
+//accesing the last element of the array
+
+console.log(marvel_Heros[marvel_Heros.length - 1]);
+
+//the last element is always the length of array - 1
+
+//during printing we can also run a js code in string 
+//with the help of `` and js code is written within ${}
+
+console.log(`we have ${marvel_Heros.length} Super Heros`);
