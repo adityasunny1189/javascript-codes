2815c8035b716c0b82367095c840bf0029c6d1a1
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-11 00:20:52 +0530

    for loops

---
+++ hithesh choudhary javascript study files/lecture7_for_loops.js
@@ -0,0 +1,12 @@
+//                                              For Loop
+
+//declaring an array and printing all its element using for loop
+
+const Days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+//looping through the array
+
+for (var i = 0; i < Days.length; i++)
+{
+    console.log(`The ${i + 1} day of week is ${Days[i]}`);
+}
