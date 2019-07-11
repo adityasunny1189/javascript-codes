034e743e9b4eabac93f3dbfd97734be4999de156
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-12 00:53:37 +0530

    left codes

--- hithesh choudhary javascript study files/forEach_loop.js
+++ hithesh choudhary javascript study files/forEach_loop.js
@@ -9,3 +9,16 @@ const month = ['jan','feb','mar','apr','may','jun','july','aug','sep','oct','nov
 month.forEach(function(month_name,index) {
     console.log(`the ${index + 1} st month is ${month_name}`);
 })
+
+
+//                                      passing function in forEach loop
+
+//making an array of days in a week
+
+const week_days = ['mon','tue','wed','thu','fri','sat'];
+
+function printing(day, index) {
+    console.log(`the ${index + 1} day is ${day}`);
+}
+
+week_days.forEach(printing);
