3bcf87e98e780c4486d554ae681d9a599d02dd01
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-12 01:01:13 +0530

    new commit on this keyword

---
+++ hithesh choudhary javascript study files/this_keyword.js
@@ -0,0 +1,32 @@
+//                                          this keyword
+
+//this keyword is generall used for methords inside object
+//this keyword is used to access to acsess object properties inside object
+
+var object = {
+    day:'monday',
+    class:0,
+    left:0,
+    attended:0,
+    add_class: function(num){
+        this.class = this.class + num;
+    },
+    attend_class: function(num){
+        this.attended = this.attended + num;
+    },
+    bunk_class: function(){
+        this.left = this.class - this.attended;
+    },
+    summary: function(){
+        console.log(`${day} total class: ${this.class} class attended: ${this.attended} class bunked: ${this.left}`);
+    },
+    reset: function(){
+        this.class = 0;
+        this.attended = 0;
+        this.left = 0;
+    }
+}
+
+object.add_class(9);
+object.attend_class(4);
+object.summary();
