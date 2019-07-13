919acc3ac4978374d9424fea2898c3363c81ea92
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-14 03:52:36 +0530

    new

---
+++ javascript_course/array_search.js
@@ -0,0 +1,26 @@
+var num1 = 24;
+var str1 = "23";
+console.log(num1 + Number(str1));
+var num2;
+console.log(num2);
+var str2 = "2554f";
+console.log(str2);
+console.log(num1 + Number(str2));
+
+//Datatypes in JavaScript
+
+var num1 = 23;
+var str1 = "aditya";
+var bool = true;
+const arr1 = [];
+var obj1 = {};
+var undef;
+var no = null;
+
+console.log(typeof(num1));
+console.log(typeof(str1));
+console.log(typeof(bool));
+console.log(typeof(arr1));
+console.log(typeof(obj1));
+console.log(typeof(undef));
+console.log(typeof(no));
