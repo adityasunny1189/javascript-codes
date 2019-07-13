65bd0c3b30500ea19fa0f1e5c72d80ca87145d34
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-13 15:26:43 +0530

    type conversion

---
+++ javascript_course/javascript_c1.js
@@ -0,0 +1,12 @@
+//In javaScript variable are dynamic
+//means no need to declare there type
+
+var num = 45;
+
+console.log(num);
+
+console.log(typeof(num));
+
+num = String(num);
+
+console.log(typeof(num));
