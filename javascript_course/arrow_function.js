f12df22d811c7fa45dcc95b997757445addb85f1
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-16 03:40:16 +0530

    new commit

---
+++ javascript_course/arrow_function.js
@@ -0,0 +1,16 @@
+//                                            Arrow Function
+
+//example of simple function
+
+const function1 = function(name){
+    return `hello ${name} welcome!`;
+}
+
+console.log(function1('Aditya'));
+
+//example of arrow function
+//Arrow function are represented by => symbol
+
+const function2 = (name) => `Hello ${name} how are you`;
+
+console.log(function2('sunny'));
