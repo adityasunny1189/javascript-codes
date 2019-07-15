2c31a3b38648bc4fa8dc2fa4ffecd9402496843b
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-15 20:24:55 +0530

    new commit

---
+++ javascript_course/type_coersion.js
@@ -0,0 +1,16 @@
+//                                              coersion
+//it is of two type 
+//implicit coersion
+//explicit coersion
+
+//explicit coersion
+
+var a = '42';
+var b = Number(a);
+console.log(typeof(b));
+
+//implicit coersion
+
+var c = '42';
+var d = c * 1;
+console.log(typeof(d));
