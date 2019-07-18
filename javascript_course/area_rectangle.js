98277d0edff560afcaeaad81dc438b62763234c5
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-18 19:28:50 +0530

    new commit

---
+++ javascript_course/area_rectangle.js
@@ -0,0 +1,8 @@
+const length = 23;
+const bredth = 34;
+
+//taking the argument and returning area through arrow function
+
+const area = (len,bre) => (len*bre);
+
+console.log(`Area of the given rectangle is ${area(length,bredth)}`);
