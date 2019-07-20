b96e9a94145cff04c3736b4097fce1f7131eaba6
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-21 04:47:49 +0530

    new commit

---
+++ javascript_course/switch_try.js
@@ -0,0 +1,9 @@
+var name1 = 'Aditya';
+var name2 = 'Rohan';
+var name3 = 'rohit';
+
+switch(name1){
+    case 'Aditya':console.log('hello aditya');break;
+    case 'Rohan':console.log('hello rohan');break;
+    case 'rohit':console.log('hello rohit');break;
+}
