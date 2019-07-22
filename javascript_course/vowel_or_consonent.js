6223742400046389ca0617b26ca2e97484ab2622
adityasunny1189 <adityapathak1189@gmail.com>
2019-07-22 22:41:10 +0530

    new commit

---
+++ javascript_course/vowel_or_consonent.js
@@ -0,0 +1,15 @@
+var alphabet = 'e';
+
+switch(alphabet){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U': console.log('Vowel');break;
+    default: console.log('consonent');
+}
